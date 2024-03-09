409aeb9198710153e22f85b73fa93f5bdf20a26a
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-09 10:50:29 +0300

    Add files via upload

---
+++ 2.unite 31 uygulama/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_31_uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = txtAra.Text;
+            for(int i=0; i<listBox1.Items.Count; i++)
+            {
+                if (listBox1.Items[i].ToString() ==ad)
+                {
+                    MessageBox.Show(listBox1.Items[i].ToString() + "    musteri bulundu");
+                    break;
+                }
+                
+
+            }
+        }
+    }
+}
